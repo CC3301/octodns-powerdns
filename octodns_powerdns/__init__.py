188005ceab29543382f10a54251a303ef7774bdf
Florian Josua Fink <ffink@kfnet.it>
2025-10-31 01:03:37 +0100

    Revert "URI unittests are failing, don't need it right now"
    This reverts commit 2420fccbf5c08a560bc18429aa73642224e61c3f.

--- octodns_powerdns/__init__.py
+++ octodns_powerdns/__init__.py
@@ -21,7 +21,12 @@ try:  # pragma: no cover
 except ImportError:  # pragma: no cover
     SUPPORTS_SVCB = False
 
-SUPPORTS_URI = False
+try:  # pragma: no cover
+    from octodns.record.uri import UriValue
+
+    SUPPORTS_URI = True
+except ImportError:  # pragma: no cover
+    SUPPORTS_URI = False
 
 
 from .record import PowerDnsLuaRecord
